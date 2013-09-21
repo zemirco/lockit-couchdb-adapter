385f606b124fcb62e07cc3c0de7a84fa538314d0
Mirco <mirco.zeiss@gmail.com>
2013-09-21 11:37:52 +0200

    fix adapter and use config from arguments

--- index.js
+++ index.js
@@ -1,76 +1,83 @@
 
-var pwd = require('pwd');
-var config = require('./config.dev.js');
+var path = require('path');
 var uuid = require('node-uuid');
+var bcrypt = require('bcrypt');
 
-var db = require('nano')({
-//  url: 'https://' + config.dbUser + ':' + config.dbPass + '@' + config.dbUrl + '',
-  url: config.dbUrl
-//  request_defaults: config.request_defaults
-});
+module.exports = function(config) {
+  
+  var db = require('nano')({
+    url: config.dbUrl,
+    request_defaults: config.request_defaults
+  });
+  
+  var adapter = {};
 
-// also return signupToken
-exports.create = function(name, email, pw, done) {
+  // also return signupToken
+  adapter.save = function(name, email, pw, done) {
 
-  // set sign up token expiration date
-  var now = new Date();
-  var tomorrow = now.setTime(now.getTime() + (config.signupTokenExpiration));
+    // set sign up token expiration date
+    var now = new Date();
+    var tomorrow = now.setTime(now.getTime() + (config.signupTokenExpiration));
 
-  var user = {
-    username: name,
-    email: email,
-    signupToken: uuid.v4(),
-    signupTimestamp: new Date(),
-    signupTokenExpires: new Date(tomorrow)
+    var user = {
+      username: name,
+      email: email,
+      signupToken: uuid.v4(),
+      signupTimestamp: new Date(),
+      signupTokenExpires: new Date(tomorrow),
+      failedLoginAttempts: 0
+    };
+
+    // create salt and hash password
+    bcrypt.hash(pw, 10, function(err, hash) {
+      if (err) return done(err);
+      user.hash = hash;
+
+      // db insert doesn't return the user -> get it manually
+      db.insert(user, function(err, res) {
+        if (err) return done(err);
+
+        db.get(res.id, done);
+      });
+
+    });
+
   };
 
-  // create salt and hash password
-  pwd.hash(pw, function(err, salt, hash){
-    if (err) return done(err);
-    user.salt = salt;
-    user.hash = hash;
+  // find a user
+  // match is either "username", "email" or "signupToken"
+  adapter.find = function(match, query, done) {
 
-    // db insert doesn't return the user -> get it manually
+    db.view('users', match, {key: query}, function(err, res) {
+      if (err) return done(err);
+      res.rows.length ? done(null, res.rows[0].value) : done(null);
+    });
+
+  };
+
+  // update an existing user object
+  adapter.update = function(user, done) {
+
     db.insert(user, function(err, res) {
       if (err) return done(err);
-      
       db.get(res.id, done);
     });
 
-  });
-  
-};
+  };
 
-// find a user
-// match is either "username", "email" or "signupToken"
-exports.find = function(match, query, done) {
+  // delete an existing user
+  adapter.delete = function(match, query, done) {
 
-  db.view('users', match, {key: query}, function(err, res) {
-    if (err) return done(err);
-    res.rows.length ? done(null, res.rows[0].value) : done(null);
-  });
-  
-};
-
-// update an existing user object
-exports.update = function(user, done) {
-
-  db.insert(user, function(err, res) {
-    if (err) return done(err);
-    db.get(res.id, done);
-  });
-  
-};
-
-// delete an existing user
-exports.delete = function(match, query, done) {
-
-  db.view('users', match, {key: query}, function(err, res) {
-    if (err) return done(err);
-    db.destroy(res.rows[0].value._id, res.rows[0].value._rev, function(err, res) {
+    db.view('users', match, {key: query}, function(err, res) {
       if (err) return done(err);
-      done(null, true);
+      db.destroy(res.rows[0].value._id, res.rows[0].value._rev, function(err, res) {
+        if (err) return done(err);
+        done(null, true);
+      });
     });
-  });
-    
+
+  };
+  
+  
+  return adapter;
 };
