ba67ac63bba243ad02c91886072804be01e20978
zeMirco <mirco.zeiss@gmail.com>
2014-04-03 19:27:35 +0200

    update tests

--- test/config.js
+++ test/config.js
@@ -1,6 +1,6 @@
 
 // database url
-exports.db = 'http://127.0.0.1:5984/test';
+exports.db = 'http://127.0.0.1:5984/';
 
 exports.signup = {
   tokenExpiration: '1 day'
--- test/test.js
+++ test/test.js
@@ -6,7 +6,7 @@ var adapter = require('../index.js')(config);
 
 // start the tests
 describe('couchdb adapter for lockit', function() {
-  
+
   // needed later for test 'find user by signup token'
   var _tmp_signupToken = '';
 
@@ -14,7 +14,6 @@ describe('couchdb adapter for lockit', function() {
 
     adapter.save('john', 'john@email.com', 'secret', function(err, res) {
       if (err) console.log(err);
-      
       res.should.have.property('signupToken');
       res.signupToken.should.match(/[0-9a-f]{22}|[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/);
       res.should.have.property('failedLoginAttempts');
@@ -26,7 +25,7 @@ describe('couchdb adapter for lockit', function() {
     });
 
   });
-  
+
   it('should find a user by username', function(done) {
 
     adapter.find('username', 'john', function(err, res) {
@@ -35,21 +34,20 @@ describe('couchdb adapter for lockit', function() {
       res.email.should.equal('john@email.com');
       done();
     });
-    
+
   });
-  
+
   it('should return undefined when no user is found', function(done) {
 
     adapter.find('username', 'jim', function(err, res) {
       if (err) console.log(err);
-      
       should.not.exist(err);
       should.not.exist(res);
       done();
     });
-    
+
   });
-  
+
   it('should find a user by email', function(done) {
 
     adapter.find('email', 'john@email.com', function(err, res) {
@@ -58,7 +56,7 @@ describe('couchdb adapter for lockit', function() {
       res.email.should.equal('john@email.com');
       done();
     });
-    
+
   });
 
   it('should find a user by signup token', function(done) {
@@ -71,38 +69,32 @@ describe('couchdb adapter for lockit', function() {
     });
 
   });
-  
+
   it('should update an existing user', function(done) {
 
     adapter.find('username', 'john', function(err, doc) {
       if (err) console.log(err);
-      
       doc.test = 'works';
       doc.editet = true;
-
       adapter.update(doc, function(err, res) {
         if (err) console.log(err);
-        
         res.test.should.equal('works');
         res.editet.should.be.true;
         done();
-        
       });
-      
+
     });
-    
+
   });
 
   it('should remove a user', function(done) {
-
     var user = {
       username: 'jeff',
       email: 'jeff@email.com'
     };
-
     adapter.save('jeff', 'jeff@email.com', 'secret', function(err, res) {
       if (err) console.log(err);
-      adapter.remove('username', 'jeff', function(err, res) {
+      adapter.remove('jeff', function(err, res) {
         if (err) console.log(err);
         res.should.be.true;
         done();
@@ -110,19 +102,17 @@ describe('couchdb adapter for lockit', function() {
     });
 
   });
-  
+
   it('should return an error when remove cannot find a user', function(done) {
-    adapter.remove('username', 'steve', function(err, res) {
-      err.message.should.equal('lockit - Cannot find username: "steve"');
+    adapter.remove('steve', function(err, res) {
+      err.message.should.equal('lockit - Cannot find user "steve"');
       done();
     });
   });
-  
+
 });
 
 // remove users db
 after(function(done) {
-
-  adapter.remove('username', 'john', done);
-      
+  adapter.remove('john', done);
 });
