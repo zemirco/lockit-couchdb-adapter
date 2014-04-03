743333733d7e00165f7706eb941511e8adf16c32
zeMirco <mirco.zeiss@gmail.com>
2014-04-03 19:25:54 +0200

    create db per user (fix #2)

--- index.js
+++ index.js
@@ -3,47 +3,57 @@ var uuid = require('node-uuid');
 var bcrypt = require('bcrypt');
 var ms = require('ms');
 var moment = require('moment');
+var Promise = require('bluebird');
+var init = require('./utils/create-views.js');
 
 module.exports = function(config) {
 
-  var db = require('nano')(config.db);
-
-  // load helper function
-  var init = require('./utils/create-views.js');
-
-  // create views
-  init(config, function(err, saved) {
-    if (err) throw err;
-  });
+  var nano = require('nano')(config.db);
 
   var adapter = {};
 
   // also return signupToken
   adapter.save = function(name, email, pw, done) {
 
-    var now = moment().toDate();
-    var timespan = ms(config.signup.tokenExpiration);
-    var future = moment().add(timespan, 'ms').toDate();
-
-    var user = {
-      username: name,
-      email: email,
-      signupToken: uuid.v4(),
-      signupTimestamp: now,
-      signupTokenExpires: future,
-      failedLoginAttempts: 0
-    };
-
-    // create salt and hash password
-    bcrypt.hash(pw, 10, function(err, hash) {
+    // create new db for user
+    var dbName = 'lockit/' + name;
+    nano.db.create(dbName, function(err, body) {
       if (err) return done(err);
-      user.hash = hash;
 
-      // db insert doesn't return the user -> get it manually
-      db.insert(user, function(err, res) {
+      var db = nano.use(dbName);
+
+      // create views
+      init(db, function(err, saved) {
         if (err) return done(err);
 
-        db.get(res.id, done);
+        // create user
+        var now = moment().toDate();
+        var timespan = ms(config.signup.tokenExpiration);
+        var future = moment().add(timespan, 'ms').toDate();
+
+        var user = {
+          type: 'user',
+          username: name,
+          email: email,
+          signupToken: uuid.v4(),
+          signupTimestamp: now,
+          signupTokenExpires: future,
+          failedLoginAttempts: 0
+        };
+
+        // create salt and hash password
+        bcrypt.hash(pw, 10, function(err, hash) {
+          if (err) return done(err);
+          user.hash = hash;
+
+          // db insert doesn't return the user -> get it manually
+          db.insert(user, function(err, res) {
+            if (err) return done(err);
+            db.get(res.id, done);
+          });
+
+        });
+
       });
 
     });
@@ -54,39 +64,65 @@ module.exports = function(config) {
   // match is either "username", "email" or "signupToken"
   adapter.find = function(match, query, done) {
 
-    db.view('lockit-users', match, {key: query}, function(err, res) {
+    function checkDbforUser(dbName) {
+      return new Promise(function (resolve, reject) {
+        var db = nano.use(dbName);
+        db.view('lockit-user', match, {key: query}, function(err, res) {
+          if (err) return reject(err);
+          if (!res.rows.length) return resolve(null);
+          resolve(res.rows[0].value);
+        });
+      });
+    }
+
+    // get all dbs
+    nano.db.list(function(err, body) {
       if (err) return done(err);
-      res.rows.length ? done(null, res.rows[0].value) : done(null);
+
+      // loop through all dbs and check for 'lockit/...'
+      var dbs = body.filter(function(db) {
+        if (/^lockit\//.test(db)) return db;
+      });
+
+      // create array with promises to query every single db
+      var promisesArray = dbs.map(function(dbName) {
+        return checkDbforUser(dbName);
+      });
+
+      // query all dbs
+      Promise.filter(promisesArray, function(result) {
+        if (result) return result;
+      })
+      .spread(function(res) {
+        done(null, res);
+      })
+      .catch(function(err) {
+        done(err);
+      });
+
     });
 
   };
 
   // update an existing user object
   adapter.update = function(user, done) {
-
+    var db = nano.use('lockit/' + user.username);
     db.insert(user, function(err, res) {
       if (err) return done(err);
       db.get(res.id, done);
     });
-
   };
 
   // delete an existing user
-  adapter.remove = function(match, query, done) {
-
-    db.view('lockit-users', match, {key: query}, function(err, res) {
+  adapter.remove = function(username, done) {
+    nano.db.destroy('lockit/' + username, function(err, res) {
+      if (err && err.status_code === 404) {
+        return done(new Error('lockit - Cannot find user "' + username + '"'));
+      }
       if (err) return done(err);
-      // no user found
-      if (!res.rows.length) return done(new Error('lockit - Cannot find ' + match + ': "' + query + '"'));
-      // delete user from db
-      db.destroy(res.rows[0].value._id, res.rows[0].value._rev, function(err, res) {
-        if (err) return done(err);
-        done(null, true);
-      });
+      done(null, res.ok);
     });
-
   };
-
 
   return adapter;
 };
