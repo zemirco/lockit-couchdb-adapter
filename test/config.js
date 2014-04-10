87e4d12bb28b15737230a55f771e310d88a38e22
zeMirco <mirco.zeiss@gmail.com>
2014-04-10 22:03:22 +0200

    add optional request defaults to config

--- test/config.js
+++ test/config.js
@@ -2,6 +2,11 @@
 // database url
 exports.db = 'http://127.0.0.1:5984/';
 
+// add requests defaults - for example
+exports.request_defaults = {
+  // proxy: 'http://someproxy'
+};
+
 exports.signup = {
   tokenExpiration: '1 day'
 };
