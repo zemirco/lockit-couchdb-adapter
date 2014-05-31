613a51d2b4bd2dad0dfa8b90eb0a10ddc5660167
zeMirco <mirco.zeiss@gmail.com>
2014-05-31 21:22:05 +0200

    improve jsdoc comments

--- index.js
+++ index.js
@@ -10,7 +10,12 @@ var init = require('./utils/create-views.js');
 /**
  * Adapter constructor function.
  *
- * @param {Object} config
+ * @example
+   var Adapter = require('lockit-couchdb-adapter');
+   var config = require('./config.js');
+   var adapter = new Adapter(config);
+ *
+ * @param {Object} config - Lockit configuration
  * @constructor
  */
 var Adapter = module.exports = function(config) {
@@ -43,10 +48,32 @@ var Adapter = module.exports = function(config) {
 /**
  * Create a new user.
  *
- * @param {String} name
- * @param {String} email
- * @param {String} pw
- * @param {Function} done
+ * @example
+   adapter.save('john', 'john@email.com', 'secret', function(err, user) {
+     if (err) console.log(err);
+     console.log(user);
+     // {
+     //  _id: '8c7cd00c55a25ceb279a8e893d011b3e',
+     //  _rev: '1-530a4cd8e67d51daf74e059899c39cd5',
+     //  password_scheme: 'pbkdf2',
+     //  iterations: 10,
+     //  name: 'john',
+     //  email: 'john@email.com',
+     //  roles: [ 'user' ],
+     //  type: 'user',
+     //  signupToken: 'fed26ce9-2628-405a-b9fa-285d4a66f4c3',
+     //  signupTimestamp: '2013-09-21T10:10:50.357Z',
+     //  signupTokenExpires: '2014-01-15T15:27:29.020Z',
+     //  failedLoginAttempts: 0,
+     //  derived_key: '0a2b1714d6017e7efdc1154ee34c805dea29c06a',
+     //  salt: '3a213c87b6a33c70fec767acea697994'
+     // }
+   });
+ *
+ * @param {String} name - User name
+ * @param {String} email - User email
+ * @param {String} pw - Plain text user password
+ * @param {Function} done - Callback function `function(err, user){}`
  */
 Adapter.prototype.save = function(name, email, pw, done) {
   var that = this;
@@ -122,9 +149,31 @@ Adapter.prototype.save = function(name, email, pw, done) {
 /**
  * Find user. Match is either `name`, `email` or `signupToken`.
  *
- * @param {String} match
- * @param {String} query
- * @param {Function} done
+ * @example
+   adapter.find('name', 'john', function(err, user) {
+     if (err) console.log(err);
+     console.log(user);
+     // {
+     //  _id: '8c7cd00c55a25ceb279a8e893d011b3e',
+     //  _rev: '1-530a4cd8e67d51daf74e059899c39cd5',
+     //  password_scheme: 'pbkdf2',
+     //  iterations: 10,
+     //  name: 'john',
+     //  email: 'john@email.com',
+     //  roles: [ 'user' ],
+     //  type: 'user',
+     //  signupToken: 'fed26ce9-2628-405a-b9fa-285d4a66f4c3',
+     //  signupTimestamp: '2013-09-21T10:10:50.357Z',
+     //  signupTokenExpires: '2014-01-15T15:27:29.020Z',
+     //  failedLoginAttempts: 0,
+     //  derived_key: '0a2b1714d6017e7efdc1154ee34c805dea29c06a',
+     //  salt: '3a213c87b6a33c70fec767acea697994'
+     // }
+   });
+ *
+ * @param {String} match - Property to find user by. `name`, `email` or `signupToken`
+ * @param {String} query - Corresponding value to `match`
+ * @param {Function} done - Callback function `function(err, user){}`
  */
 Adapter.prototype.find = function(match, query, done) {
   var that = this;
@@ -153,8 +202,24 @@ Adapter.prototype.find = function(match, query, done) {
 /**
  * Update existing user.
  *
- * @param {Object} user
- * @param {Function} done
+ * @example
+   // get user from db
+   adapter.find('name', 'john', function(err, user) {
+     if (err) console.log(err);
+
+     // add some new properties to our existing user
+     user.newKey = 'and some value';
+     user.hasBeenUpdated = true;
+
+     // save updated user to db
+     adapter.update(user, function(err, user) {
+       if (err) console.log(err);
+       // ...
+     });
+   });
+ *
+ * @param {Object} user - Existing user from db
+ * @param {Function} done - Callback function `function(err, user){}`
  */
 Adapter.prototype.update = function(user, done) {
   var that = this;
@@ -170,8 +235,15 @@ Adapter.prototype.update = function(user, done) {
 /**
  * Delete existing user.
  *
- * @param {String} name
- * @param {Function} done
+ * @example
+   adapter.remove('john', function(err, res) {
+     if (err) console.log(err);
+     console.log(res);
+     // true
+   });
+ *
+ * @param {String} name - User name
+ * @param {Function} done - Callback function `function(err, res){}`
  */
 Adapter.prototype.remove = function(name, done) {
   var that = this;
